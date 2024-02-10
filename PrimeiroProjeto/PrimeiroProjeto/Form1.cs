b63d194a649bd6b83b66559fe986d1931397e0f6
Gustavo Pirondi <gustavopirondi@gmail.com>
2024-02-09 22:11:06 -0300

    Primeiro Projeto
    Criação do Primeiro Projeto em C#

---
+++ PrimeiroProjeto/PrimeiroProjeto/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeiroProjeto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int idade;
+            idade = int.Parse(textIdade.Text);
+            if(idade >= 18){
+                MessageBox.Show("Maior de Idade!");
+            }
+            else{
+                MessageBox.Show("Menor de Idade!");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
